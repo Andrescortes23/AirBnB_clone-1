74bbb37d802e7cf5d4a36ad8bbf8ea462c63a340
Andrescortes23 <andresccortes23@icloud.com>
2020-12-30 03:12:09 +0000

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,8 @@
 from time import strftime
 from datetime import date
 from fabric.api import local, env, run, put
+from os import path
+
 
 env.hosts = ['35.237.92.39', '35.231.86.111']
 env.user = 'ubuntu'
@@ -28,17 +30,25 @@ def do_deploy(archive_path):
     distributes an archive to your web servers, using the function do_deploy
     """
 
-    if archive_path:
-        put(archive_path, "/tmp/")
-        the_file = archive_path.replace(".tgz", "")
-        run("tar xvzf archive_path -C /data/web_static/releases/{}".format
-            (the_file))
-        with cd('/tmp/'):
-            sudo('rm -f archive_path')
-        with cd('/data/web_static/'):
-            sudo('rm -f current')
-        run("ln -s /data/web_static/releases/web_static /data/web_static\
-        /current")
-        return True
+    if path.exists(archive_path):
+        try:
+            put(archive_path, "/tmp/")
+            file_path = archive_path.split('/')[-1]
+            the_file = file_path.replace(".tgz", "")
+            run("sudo mkdir -p /data/web_static/releases/{}".format
+                (the_file))
+            run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}".format
+                (file_path, the_file))
+            run("sudo rm -f /tmp/{}".format(the_file))
+            run("sudo mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(the_file, the_file))
+            run("sudo rm -rf /data/web_static/releases/{}/web_static"
+                .format(the_file))
+            run("sudo rm -rf /data/web_static/current")
+            run("sudo ln -s /data/web_static/releases/{}\
+            /data/web_static/current".format(the_file))
+            return True
+        except:
+            return False
     else:
         return False
