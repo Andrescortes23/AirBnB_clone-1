40e79bc115718aefad13567b5ed6acf151f5913b
Andrescortes23 <andresccortes23@icloud.com>
2021-01-20 01:11:54 +0000

    Task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -24,7 +24,7 @@ def home2():
 @app.route("/c/<text>", strict_slashes=False)
 def homeC(text):
     """Passing variable"""
-    return ("C {}".format(b.replace('_', ' ')))
+    return ("C {}".format(text.replace('_', ' ')))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
