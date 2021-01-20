d5d9fa4d161fde52f3e061de56927c86ef6a5336
Andrescortes23 <andresccortes23@icloud.com>
2021-01-20 02:15:43 +0000

    Task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """To show Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def home2():
+    """To show HBNB"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def homeC(text):
+    """Passing variable"""
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def homePython(text="is cool"):
+    """Passing variable with default value"""
+    return ("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def homeInt(n):
+    """Passing int varaible"""
+    return ("{} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def homeIntTemplate(n):
+    """Display HTML page"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
