790b320792ac889d835318c0210bcc34dbb7f761
Andrescortes23 <andresccortes23@icloud.com>
2021-01-20 20:26:10 +0000

    Task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,14 +8,20 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-app = Flask(__namne__)
+app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def the_states():
     """HTML with the states"""
-    return render_template('7-states_list.html', states=storage.all(State))
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
+
+@app.teardown_appcontext
+def close(self):
+    """Close Session"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
