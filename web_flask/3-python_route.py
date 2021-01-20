c7aa22d63a9acfd1dd87990bb6554fd95fa0cbd0
Andrescortes23 <andresccortes23@icloud.com>
2021-01-20 01:20:51 +0000

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """To show Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def home2():
+    """To show HBNB"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def homeC(text):
+    """Passing variable"""
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def homePython(text="is fun"):
+    """Passing variable with default value"""
+    return ("Python {}".format(text.replace('_', ' ')))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
